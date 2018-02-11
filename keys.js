92609af3a1c5171b8bbb6c1b0cabf8da01a9c0a5
Luke Stahl <Stahl1109@gmail.com>
2018-02-11 13:18:27 -0600

    install node packages

--- keys.js
+++ keys.js
@@ -15,3 +15,14 @@ exports.spotify = {
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+
+
+
+//npm twitter
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
