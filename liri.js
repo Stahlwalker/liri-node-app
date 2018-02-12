bf19bb775851ae0a9bac18b5bc67023401fd0165
Luke Stahl <Stahl1109@gmail.com>
2018-02-12 10:08:20 -0600

    code from mulitple sources

--- liri.js
+++ liri.js
@@ -4,45 +4,80 @@ var fs = require("fs");
 var keys=require("./keys.js");
 var twitter = require("twitter");
 var spotify = require("spotify");
-
-//npm omdb
-// Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-var request = require("request");
-
-// Then run a request to the OMDB API with the movie specified
-request("http://www.omdbapi.com/?i=tt3896198&apikey=392b0493", function(error, response, body) {
-
-  // If the request is successful (i.e. if the response status code is 200)
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-  }
-});
-
-
-var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
-
-
-
+var spotify = new Spotify(keys.spotify);
+var first_argv = process.argv;
+var second_argv = process.argv[2];
 
 //npm twitter
-var params = {screen_name: 'nodejs'};
+
+function displayTweets(){
+
+var params = {screen_name: 'PackersStahl'};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    for(var i =0; i<tweets.length; i++){
+      var date = tweets[i].created_at;
+      console.log("@PackersStahl: " + tweets[i].text + "Created at: " + date.substring(0, 19));
+      console.log("\n----------\n");
+
+      fs.appendFile('log.txt', "@PackersStahl: " + tweets[i].text + "Created at: " + date.substring(0, 19));
+      fs.appendFile('log.txt', "\n----------\n");
+    }
+  }else{
+    console.log('Error occured');
   }
 });
+}
 
-client.get(path, params, callback);
+
 
 //npm spotify
-spotify.search({ type: 'track', query: 'I Want it That Way' }, function(err, data) {
+function displaySongs(song){
+spotify.search({ type: 'track', query: song}, function(err, data) {
   if (err) {
+    for(var i = 0; i < data.tracks.items.length; i++){
+      var songInfo = data.tracks.items[i];
+      console.log("Artist: " + songInfo.artists[0].name);
+      console.log("Song: " + songInfo.name);
+      console.log("Album: " + songInfo.album.nanme);
+      console.log("\n----------\n");
+
+      fs.appendFile('log.txt', songInfo.artist[0].name);
+      fs.appendFile("Song: " + songInfo.name);
+      fs.appendFile("Album: " + songInfo.album.nanme);
+      fs.appendFile("\n----------\n");
+    }
     return console.log('Error occurred: ' + err);
   }
  
-console.log(data); 
 });
+}
+
+
+//npm omdb
+
+function movieInfo(movie) {
+  var request = require("request");
+  request("http://www.omdbapi.com/?i=tt3896198&apikey=392b0493", function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var movieInfo = JSON.parse(body);
+      if (movieDetails.Response === 'False') {
+        movieInfo('Mr. Nobody');
+        console.log("If you haven't watched Mr. Nobody, then you should http://www.imdb.com/title/tt0485947/, it's also on Netflix " + JSON.parse(body).imdbRating);
+      }
+      else {  
+        console.log("Title: " + JSON.parse(body).imdbTitle);
+        console.log("Release Year: " + JSON.parse(body).imdbReleased);
+        console.log("Rating: " + JSON.parse(body).imdbRated);
+        console.log("Rotten Tomatoes bogus rating: " + JSON.parse(body).imdbRatings);
+        console.log("Country where produced: " + JSON.parse(body).imdbCountry);
+        console.log("Language: " + JSON.parse(body).imdbLanguage);
+        console.log("Plot: " + JSON.parse(body).imdbPlot);
+        console.log("Actors: " + JSON.parse(body).imdbActors);
+        console.log("\n----------\n");
+      }
+
+    }
+  });
+}
